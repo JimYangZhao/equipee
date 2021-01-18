6e797e5b9c90c3ea59085e1fc5f99833ba3004ac
JimYangZhao <yangzhao264@gmail.com>
2021-01-18 10:13:02 -0330

    weekends work, Jan18th

--- src/Banner.js
+++ src/Banner.js
@@ -1,24 +1,28 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import { Button } from "@material-ui/core";
+import Search from './Search';
+import { useHistory } from "react-router-dom";
 
 function Banner() {
+    const history = useHistory();
     const [showSearch, setShowSearch] = useState(false);
 
     return (
         <div className='banner'>
-            <div className='banner_search'>
-                {showSearch && <h1>Show Date Picker</h1>}
-                <Button onClick={() =>setShowSearch(!showSearch)} className='banner_searchButton' variant='outlined'>
-                    Search Dates
+            <div className='banner__search'>
+                {showSearch && <Search />}
+
+                <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>
+                    {showSearch ? "Hide" : "Search Dates"}
                 </Button>
             </div>
-            <div className='banner_info'>
-                <h1>Get out and strech you imagination</h1>
+            <div className='banner__info'>
+                <h1>Get out and stretch your imagination</h1>
                 <h5>
-                    Plan a different kind of get away to uncover the hidden gems near you
+                    Plan a different kind of getaway to uncover the hidden gems near you.
                 </h5>
-                <Button variant='outlined'>Explore Nearby</Button>
+                <Button onClick={() => history.push('/search')} variant='outlined'>Explore Nearby</Button>
             </div>
         </div>
     )
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,66 @@
 import React from 'react';
 import './Home.css';
 import Banner from './Banner'
+import Card from './Card'
+
+// ES7 snippets to do 'rfce'
 
 function Home() {
     return (
         <div className='home'>
             <Banner />
+
+            <div className='home__section'>
+            <h1>Our Services</h1>
+            
+            </div>
+            <div className='home__section'>
+            <Card
+                src="http://equipmee.com/wp-content/uploads/2021/01/Wheel-loader-495x400.jpg"
+                title="Service#1"
+                description="An ecommerce solution for rental companies to enable touchless online bookings with customers."
+            />
+            <Card
+                src="http://equipmee.com/wp-content/uploads/2021/01/volvo-find-soil-compactor-sd135b-t4f-walkaround-1000x1000-1-495x400.jpg"
+                title="Service#2"
+                description="An ecommerce solution for rental companies to enable touchless online bookings with customers."
+            />
+            <Card
+                src="http://equipmee.com/wp-content/uploads/2021/01/PC210LC-11_1-495x375.png"
+                title="Service#3"
+                description="An ecommerce solution for rental companies to enable touchless online bookings with customers."
+            />
+            </div>
+
+            <div className='home__section'>
+            <h1>POPULAR EQUIPMENT RENTALS</h1>
+            
+            </div>
+
+            <div className='home__section'>
+            <Card
+                src="http://equipmee.com/wp-content/uploads/2021/01/Gehl-845x684.jpg"
+                title="Equipment#1"
+                description="An ecommerce solution for rental companies to enable touchless online bookings with customers."
+                price="$130/day"
+            />
+            <Card
+                src="http://equipmee.com/wp-content/uploads/2021/01/CM20180928-35976-27789-495x400.jpeg"
+                title="Equipment#2"
+                description="An ecommerce solution for rental companies to enable touchless online bookings with customers."
+                price="$350/day"
+            />
+            <Card
+                src="http://equipmee.com/wp-content/uploads/2021/01/3cx-02-1000-495x400.jpg"
+                title="Equipment#3"
+                description="An ecommerce solution for rental companies to enable touchless online bookings with customers."
+                price="$70/day"
+            />
+            </div>
+            <div className='home__section'>
+            <h1>More</h1>
+            
+            </div>
         </div>
     )
 }
